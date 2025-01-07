b602f24ba161d22e650050e1795e3566e8bd33b6
Jatin Chandra Choudhary <98642385+RobinTheHooter@users.noreply.github.com>
2025-01-07 16:29:19 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio",
-  description: "Created with the help of Frontend Tribe",
+  title: "Jatin Choudhary",
+  description: "Powered by NextJS",
 };
 
 export default function RootLayout({
